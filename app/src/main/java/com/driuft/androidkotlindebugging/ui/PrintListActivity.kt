5a7558161c4b5b08fa020a9621e08cf751f98ffa
Terence Lin <tlin41@dons.usfca.edu>
2025-06-17 02:06:08 -0700

    fixed bugs

--- app/src/main/java/com/driuft/androidkotlindebugging/ui/PrintListActivity.kt
+++ app/src/main/java/com/driuft/androidkotlindebugging/ui/PrintListActivity.kt
@@ -26,8 +26,8 @@ class PrintListActivity : AppCompatActivity() {
 
     private fun combinedWords(): String {
         var combined = ""
-        for (idx in 0 until words.size + 1) {
-            combined += "${words[idx]}\n"
+        for (element in words) {
+            combined += "${element}\n"
         }
         return combined
     }
